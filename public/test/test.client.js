038a3dbdaef99b9b2134dd546450f291ebd28d1e
cmilhench <colinm@cm-group.co.uk>
2013-11-08 13:04:28 +0000

    add browser tests

---
+++ public/test/test.client.js
@@ -0,0 +1,12 @@
+// -----------------------------------------------------------------
+//  test.client.js
+//  
+//  talking-drum
+//  Copyright 2013. Colin Milhench. All rights reserved.
+//  
+//  Colin Milhench
+// 
+
+
+describe('Client', function(){
+});
---
+++ public/test/test.parser.js
@@ -0,0 +1,38 @@
+// -----------------------------------------------------------------
+//  test.parser.js
+//  
+//  talking-drum
+//  Copyright 2013. Colin Milhench. All rights reserved.
+//  
+//  Colin Milhench
+// 
+
+
+describe('Parser', function(){
+  it('should emit "message" events', function(done){
+    var parser = new window.Parser();
+    var n = 0;
+
+    parser.on('message', function(msg){
+      switch (n++) {
+        case 0:
+          msg.command.should.equal('nick');
+          msg.params.should.equal('cmilhench');
+          break;
+        case 1:
+          msg.command.should.equal('');
+          msg.string.should.equal('Hello world\r\n');
+          break;
+        case 2:
+          msg.command.should.equal('join');
+          msg.params.should.equal('#express');
+          done();
+          break;
+      }
+    });
+
+    parser.line('/nick cmilhench\r\n');
+    parser.line('Hello world\r\n');
+    parser.line('/join #express\r\n');
+  })
+})
